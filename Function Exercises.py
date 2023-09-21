9ae12630ce9c7ec32dce87ebaa1803b5f5dad6cc
Michael Drinkard <MichaelDavidDrinkard@gmail.com>
2023-09-20 18:26:09 -0700

    This is my 3rd Commit

---
+++ Function Exercises.py
@@ -0,0 +1,70 @@
+"""
+Determine the Compound Annual Growth Rate for an investment
+"""
+
+# Declare a variable beginning_balance as a float
+
+
+# Declare a variable ending_balance as float
+
+
+# Declare a variable years as an float
+
+
+# Define a function called calculate_compound_growth_rate with three arguments: beginning_balance, ending_balance, years. Function should output growth_rate.
+
+
+
+
+
+# Call calculate_compound_growth_rate using beginning_balance, ending_balance, and years. Capture as year_one_growth.
+
+
+
+# Update beginning_balance and ending balance for year two, and then execute calculate_compound_growth_rate
+
+
+
+
+# Call calculate_compound_growth_rate using beginning_balance, ending_balance, and years. Capture as year_two_growth.
+
+
+# Update beginning_balance and ending balance for year three, and then execute calculate_compound_growth_rate
+
+
+
+# Call calculate_compound_growth_rate using beginning_balance, ending_balance, and years. Capture as year_three_growth.
+
+
+
+# Use Python round() function to round year_one_growth, year_two_growth, and year_three_growth. Capture these as new variables.
+
+
+
+# Print year_one_growth, year_two_growth, year_three_growth as percents using string formatting
+
+
+# Challenge
+
+# Create a global, empty list
+
+
+# Define a function called
+
+
+    # Populate growth_rates list using add() function
+
+
+# Call calculate_compound_growth_rate_list and populate growth_rates with 2016 values (beginning_balance and ending_balance)
+
+
+
+# Call calculate_compound_growth_rate_list and populate growth_rates with 2017 values (beginning_balance and ending_balance)
+
+
+
+# Call calculate_compound_growth_rate_list and populate growth_rates with 2018 values (beginning_balance and ending_balance)
+
+
+
+# Print growth_rates list
